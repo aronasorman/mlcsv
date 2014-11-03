6685ca688c5a7fc2831aaf8bd75f56d3287c0f79
Aron Fyodor Asor <aronasorman@gmail.com>
2014-11-03 15:43:39 -0800

    add script to move csvs from Downloads to data

---
+++ data/move_stuff.sh
@@ -0,0 +1,3 @@
+ mv ~/Downloads/New\ Playlists\ Post\ MY\ -\ Grade\ 4.csv grade4.csv
+ mv ~/Downloads/New\ Playlists\ Post\ MY\ -\ Grade\ 5.csv grade5.csv
+ mv ~/Downloads/New\ Playlists\ Post\ MY\ -\ Grade\ 6.csv grade6.csv
